0edcb99e396d81fc02bd0710532a5c39bd8650e3
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2021-10-21 16:07:52 +0100

    Refactor(Display books based on book ID)

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 (function () {
   'use strict';
 
+  /* DOCUMENT SELECTORS */
   const newBookForm = document.querySelector('#new-book-form');
   const shelves = document.querySelector('#shelves');
+  const bookIds = () => document.querySelectorAll('.book-id');
 
-  let library = [
-    {
-      title: 'test',
-      author: 'author',
-      pages: 123,
-      hasRead: false
-    },
-    {
-      title: 'test',
-      author: 'author',
-      pages: 345,
-      hasRead: true
-    }
-  ];
+  const library = () => localStorage.getItem('library') ? JSON.parse(localStorage.getItem('library')) : [];
 
-  function Book(title, author, pages, hasRead) {
+  function generateBookID() {
+    const array = new Uint32Array(1);
+    const uuid = window.crypto.getRandomValues(array)[0];
+
+    return uuid.toString();
+  }
+
+  function Book(id, title, author, pages, hasRead) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -30,17 +27,15 @@
     const formElems = evt.target.elements;
     const newBook = Object.create(Book);
     
+    newBook.id = generateBookID();
     newBook.title = formElems.title.value;
     newBook.author = formElems.author.value;
     newBook.pages = formElems.pages.value;
     newBook.hasRead = formElems['has-read'].checked;
 
-    library = library.concat(newBook);
-  }
+    const newLibrary = [...library(), newBook];
 
-  function addBookToDisplay() {
-    const newBook = library.slice(-1)[0];
-    shelves.append(createBookElem(newBook));
+    localStorage.setItem('library', JSON.stringify(newLibrary));
   }
 
   function createBookElem(book) {
@@ -48,14 +43,24 @@
     bookElem.classList.add('book');
 
     for (let prop in book) {
-      bookElem.innerHTML += `<p>${prop}: ${book[prop]}</p>`
+      prop === 'id' 
+        // Attach a hidden book ID to the book element
+        ? bookElem.innerHTML += `<p class="book-id" hidden>${book[prop]}</p>`
+        : bookElem.innerHTML += `<p>${prop}: ${book[prop]}</p>`
     }
 
     return bookElem;
   }
 
   function displayBooks() {
-    library.forEach(book => shelves.append(createBookElem(book)));
+    if (bookIds().length > 0) {
+      const bookIdsOnDisplay = Array.from(bookIds()).map(id => id.textContent);
+      const missingBooks = library().filter(book => !bookIdsOnDisplay.includes(book.id));
+
+      missingBooks.forEach(book => shelves.prepend(createBookElem(book)));
+    } else {
+      library().forEach(book => shelves.prepend(createBookElem(book)));
+    }
   }
 
   displayBooks();
@@ -63,6 +68,6 @@
   newBookForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     addBookToLib(evt);
-    addBookToDisplay();
+    displayBooks();
   });
 }());
